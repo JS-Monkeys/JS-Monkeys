3b8ec16b7b779d4e92fee761277526c3766cd4cc
KonstantinSimeonov <kon.simeonov@gmail.com>
2016-01-05 16:54:38 +0200

    Removed redundant code in user services

--- source/server/data/data-services/users-services.js
+++ source/server/data/data-services/users-services.js
@@ -4,79 +4,82 @@
     let mongoose = require('mongoose'),
         encryption = require('../../utils/encryption'),
         User = mongoose.model('User');
-    
-    // TODO: remove redundant code
+
+    function findUser(options) {
+
+        options = options || {};
+
+        if (options.id) {
+            options._id = options.id;
+            options.id = undefined;
+        }
+
+        let promise = new Promise(function (resolve, reject) {
+
+            User.findOne(options, function (error, users) {
+                if (error) {
+                    return reject(error);
+                }
+
+                resolve(users);
+            });
+
+        });
+
+        return promise;
+    }
+
+    function getUsers(options) {
+
+        options = options || {};
+
+        let promise = new Promise(function (resolve, reject) {
+
+            User.find(options, function (error, users) {
+                if (error) {
+                    return reject(error);
+                }
+
+                resolve(users);
+            });
+
+        });
+
+        return promise;
+    }
+
+    function createUser(user) {
+
+        let salt = encryption.generateSalt();
+        let dbUser = {
+            username: user.username,
+            salt: salt,
+            passHash: encryption.hashPassword(salt, user.password)
+        };
+
+        let promise = new Promise(function (resolve, reject) {
+            User.create(dbUser, function (error, createdUser) {
+                if (error) {
+                    return reject(error);
+                }
+
+                resolve(createdUser);
+            });
+        });
+
+        return promise;
+    }
+
     module.exports = {
         name: 'users',
         services: {
-            all: function () {
-
-                let promise = new Promise(function (resolve, reject) {
-
-                    User.find({}, function (error, users) {
-                        if (error) {
-                            return reject(error);
-                        }
-
-                        resolve(users);
-                    });
-
-                });
-
-                return promise;
-            },
-            createUser: function (user) {
-
-                let salt = encryption.generateSalt();
-                let dbUser = {
-                    username: user.username,
-                    salt: salt,
-                    passHash: encryption.hashPassword(salt, user.password)
-                };
-
-                let promise = new Promise(function (resolve, reject) {
-                    User.create(dbUser, function (error, createdUser) {
-                        if (error) {
-                            return reject(error);
-                        }
-
-                        resolve(createdUser);
-                    });
-                });
-
-                return promise;
-            },
+            all: getUsers,
+            createUser: createUser,
             byUsername: function (username) {
-
-                let promise = new Promise(function (resolve, reject) {
-
-                    User.findOne({ username }, function (error, users) {
-                        if (error) {
-                            return reject(error);
-                        }
-
-                        resolve(users);
-                    });
-
-                });
-
-                return promise;
+                return findUser({ username });
             },
             byId: function (id) {
-                
-                let promise = new Promise(function (resolve, reject) {
-
-                    User.findOne({ _id: id }, function (error, users) {
-                        if (error) {
-                            return reject(error);
-                        }
-
-                        resolve(users);
-                    });
-
-                });
-
-                return promise;
+                return findUser({ _id: id });
             }
         }
     };
