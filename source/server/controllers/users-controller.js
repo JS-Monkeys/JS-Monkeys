c176fed6d573d6fc59e41856154b7cabc2f593ed
Ivo Paounov <ivo.paunov@gmail.com>
2016-01-08 16:58:41 +0200

    User register and login views

--- source/server/controllers/users-controller.js
+++ source/server/controllers/users-controller.js
@@ -1,26 +1,51 @@
 (function () {
-    'use strict';
-    
-    let data = require('../data/data');
-    
-    module.exports = {
-        registerUser: function (req, res) {
-            data.users
-                    .createUser(req.body)
-                    .then(function (response) {
-                        res.json(response);
-                    }, function (error) {
-                        res.json(error);
-                    });
-        },
-        all: function (req, res) {
-            data.users
-                    .all()
-                    .then(function (response) {
-                        res.json(response);
-                    }, function (error) {
-                        res.json(error);
-                    });
-        }
-    };
-} ());
+  'use strict';
+
+  let data = require('../data/data');
+
+  let CONTROLLER_NAME = 'users';
+
+  module.exports = {
+    registerUser: function (req, res) {
+      data.users
+        .createUser(req.body)
+        .then(function (response) {
+          res.json(response);
+        }, function (error) {
+          res.json(error);
+        });
+    },
+    all: function (req, res) {
+      data.users
+        .all()
+        .then(function (response) {
+          res.json(response);
+        }, function (error) {
+          res.json(error);
+        });
+    },
+    getRegister: function (req, res, next) {
+      res.render(CONTROLLER_NAME + '/register')
+    },
+    postRegister: function (req, res) {
+      console.log("in reg");
+      console.log(req.body);
+      console.log("in reg");
+      data.users
+        .createUser(req.body)
+        .then(function (user) {
+          req.logIn(user, function(err) {
+            if (err) {
+              res.status(400);
+              return res.send({reason: err.toString()}); // TODO
+            }
+            else {
+              res.redirect('/');
+            }
+          });
+        }, function (error) {
+          res.json(error);
+        });
+    }
+  };
+}());
--- source/server/routers/users-router.js
+++ source/server/routers/users-router.js
@@ -1,17 +1,19 @@
 (function () {
-    'use strict';
-    
-    let router = require('express').Router(),
-        usersController = require('../controllers/users-controller'),
-        auth = require('../config/auth');
-    
-    // TODO: refactor those routes
-    router.get('/api/users', usersController.all)
-          .post('/api/users', usersController.registerUser)
-          .post('/login', auth.login)
-          .post('/logout', auth.logout);
-    
-    module.exports = function (server) {
-        server.use('/', router);
-    }
-} ());
+  'use strict';
+
+  let router = require('express').Router(),
+    usersController = require('../controllers/users-controller'),
+    auth = require('../config/auth');
+
+  // TODO: refactor those routes
+  router.get('/api/users', usersController.all)
+    .get('/register', usersController.getRegister)
+    .post('/register', usersController.postRegister)
+    .get('/login', auth.login)
+    .post('/login', auth.login)
+    .post('/logout', auth.logout);
+
+  module.exports = function (server) {
+    server.use('/', router);
+  }
+}());
