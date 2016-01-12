0109a46815e5b9625319dd8523888b84c5380193
Alex-Bubblemaster <shiatzu@gmail.com>
2016-01-12 20:34:44 +0200

    Added not authorized route

--- source/server/routers/contests-router.js
+++ source/server/routers/contests-router.js
@@ -7,10 +7,10 @@ let submissionsController = require('../controllers/submissions-controller')(req
 router.get('/:name', contestsController.byName)
       .get('/:name/addproblem', auth.isInRole('admin'), contestsController.addProblemPage)
       .post('/:name/addproblem', auth.isInRole('admin'), contestsController.addProblemToContest)
-      .post('/:name', submissionsController.makeSubmission)
+      .post('/:name', auth.isAuthenticated, submissionsController.makeSubmission)
         .get('/', contestsController.all)
-        .post('/', contestsController.create)
-        .post('/add', contestsController.createJsonResponse);
+        .post('/', auth.isInRole('admin'), contestsController.create)
+        .post('/add', auth.isInRole('admin'), contestsController.createJsonResponse);
 
 
 module.exports = function (server) {
