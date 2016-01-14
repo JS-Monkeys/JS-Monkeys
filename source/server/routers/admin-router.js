422f367815ff3819b81bc273d491a0918dea30ff
KonstantinSimeonov <kon.simeonov@gmail.com>
2016-01-14 11:09:44 +0200

    Deleted unnecessary admin controller

--- source/server/routers/admin-router.js
+++ source/server/routers/admin-router.js
@@ -1,21 +1,18 @@
 'use strict';
 
 let router = require('express').Router(),
-  auth = require('../config/auth'),
-  adminController = require('../controllers/admin-controller')(require('../data/data'));
+    auth = require('../config/auth');
 
 // TODO: auth middleware
-router.get('/upload', auth.isInRole('admin'), adminController.uploadPage)
-  .get('/problems', auth.isInRole('admin'), adminController.getProblems)
-  .post('/problems', auth.isInRole('admin'), adminController.addProblem)
-  .get('/add-contest',auth.isInRole('admin'), function (req, res) {
-    let options = {
-      isAuthenticated: req.isAuthenticated(),
-      user: req.user
-    };
-    res.render('../views/contest/add', req);
-  });
+router
+    .get('/add-contest', auth.isInRole('admin'), function (req, res) {
+        let options = {
+            isAuthenticated: req.isAuthenticated(),
+            user: req.user
+        };
+        res.render('../views/contest/add', req);
+    });
 
 module.exports = function (server) {
-  server.use('/admin', router);
+    server.use('/admin', router);
 };
